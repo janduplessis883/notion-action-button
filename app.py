3931d0aae987bb176b234496e374b59b70c7b301
Jan du Plessis <drjanduplessis@icloud.com>
2024-10-31 22:47:03 +0000

    push

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     layout="wide", initial_sidebar_state="collapsed", page_title="notion-action-button", page_icon=":material/sync:"
 )
 
-action = st.button("Sync NHS Numbers")
+action = st.button("Sync DM + SMI Registers")
 if action:
     with st.spinner("Sync NHS numbers..."):
         nh = NotionHelper()
@@ -24,47 +24,29 @@ if action:
         conn = st.connection("gsheets", type=GSheetsConnection)
 
         dm_register = conn.read(worksheet="dm_register")
-        st.dataframe(dm_register)
-
         smi_register = conn.read(worksheet="smi_register")
-        st.dataframe(smi_register)
 
         dm_notion = nh.get_all_pages_as_dataframe(notion_diabetes)
         smi_notion = nh.get_all_pages_as_dataframe(notion_smi)
 
-        st.dataframe(dm_notion)
-        st.dataframe(smi_notion)
+        dm_intervention = dm_register[~dm_register['NHS number'].isin(dm_notion['NHS number'])]
+        smi_intervention = smi_register[~smi_register['NHS number'].isin(smi_notion['NHS number'])]
 
+        # Convert DataFrames to CSV
+        dm_csv = dm_intervention.to_csv(index=False)
+        smi_csv = smi_intervention.to_csv(index=False)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Add download buttons for CSVs
         st.download_button(
-            label="Download Empty CSV",
-            file_name="empty.csv",
-            data="text/csv"
+            label="Download DM Intervention CSV",
+            data=dm_csv,
+            file_name="dm_intervention.csv",
+            mime="text/csv"
         )
 
-
-
-
-
-
-
-
-
-
-settings = st.checkbox('Settings')
-if settings:
-    st.write("DM + SMI Register - Google Sheet")
-    st.write("Notion Databases fixed IDs")
+        st.download_button(
+            label="Download SMI Intervention CSV",
+            data=smi_csv,
+            file_name="smi_intervention.csv",
+            mime="text/csv"
+        )
